02d1cce7b5aaf645f67ebe6e2d089975e3700d07
ccard <ccard@mymail.mines.edu>
2013-03-26 20:07:20 -0600

    added stack class to contain the stack for the calculator

---
+++ Stack.f95
@@ -0,0 +1,19 @@
+!--------------------------------------------------------------------------
+!  Header section:
+!  TITLE: Stack
+!  AUTHOR: Chris Card, Mark Shivers
+!  CLASS:  CSCI260A
+!  DATE WRITTEN: 3/26/13
+!  LAST REVISION: 2/26/13
+!  DESCRIPTION: This is a modul containing the stack functions and
+!				Subroutines
+!  VARIABLES USED:
+!       NAME:           TYPE:           COMMENT:
+!
+!--------------------------------------------------------------------------
+
+
+
+REAL FUNCTION pop()
+
+END FUNCTION
