d0c33748aea22881baea6b5bfd43d259fcffe7aa
ccard <ccard@mymail.mines.edu>
2013-04-12 08:49:40 -0600

    added another function to convert character to in

--- Calculator.f95
+++ Calculator.f95
@@ -25,10 +25,6 @@ CHARACTER(len=30)::toParse
 INTEGER::toStack
 
 
-
-
-
-
 !Begin program
 
 
@@ -40,6 +36,22 @@ CONTAINS
 
 INTEGER FUNCTION toINT(line)
 	CHARACTER(len=30),INTENT(IN)::line
+	INTEGER::size,currPower,i,ret
+
+	size = LEN(TRIM(line))
+	currPower = 10**(size-1)
+	ret = 0
+
+	DO i=0,LEN(TRIM(line))
+		ret = currPower*()
+
+	END DO
+
+
+END FUNCTION
+
+INTEGER FUNCTION getint(char)
+	CHARACTER(len=1)::char
 
 END FUNCTION
 
