bb188b07e7dc66e5f60c9145f290fc28895c76a5
ccard <ccard@mymail.mines.edu>
2013-04-19 08:20:50 -0600

    it is printing the file out now it just needs to be formated

--- Vectorart.f95
+++ Vectorart.f95
@@ -1,9 +1,29 @@
 MODULE art
 CONTAINS
 SUBROUTINE answeroftheuniverse()
-OPEN Answer_to_42.txt
-READ(*,*)Answer_to_42.txt
+IMPLICIT NONE
 
+INTEGER::err
+CHARACTER(len=110)::input
 
+OPEN (unit=44,File = "Answer_to_42.txt", status="old", action='READ', IOSTAT = err)
+
+IF(err /= 0) THEN
+	WRITE(*,*) "Failed to open the Answer_to_42.txt file"
+ELSE
+
+DO
+
+READ(44,55,IOSTAT = err) input
+55 FORMAT(A110)
+IF(err /= 0) THEN
+	EXIT
+END IF
+
+WRITE(*,*) input
+
+END DO
+CLOSE(44)
+END IF
 END SUBROUTINE
 END MODULE
