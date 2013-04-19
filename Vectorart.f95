b43bf19009f948f76ea5f318175100d514b36794
ccard <ccard@mymail.mines.edu>
2013-04-19 08:26:22 -0600

    formatted the file

--- Vectorart.f95
+++ Vectorart.f95
@@ -1,29 +1,30 @@
 MODULE art
 CONTAINS
 SUBROUTINE answeroftheuniverse()
-IMPLICIT NONE
+	IMPLICIT NONE
+	
+	INTEGER::err
+	CHARACTER(len=110)::input
+	
+	OPEN (unit=44,File = "Answer_to_42.txt", status="old", action='READ', IOSTAT = err)
+	
+	IF(err /= 0) THEN
+		WRITE(*,*) "Failed to open the Answer_to_42.txt file"
+		
+	ELSE
+	
+		DO
+			READ(44,55,IOSTAT = err) input
+			55 FORMAT(A110)
 
-INTEGER::err
-CHARACTER(len=110)::input
+			IF(err /= 0) THEN
+				EXIT
+			END IF
+			
+			WRITE(*,*) input
+		END DO
 
-OPEN (unit=44,File = "Answer_to_42.txt", status="old", action='READ', IOSTAT = err)
-
-IF(err /= 0) THEN
-	WRITE(*,*) "Failed to open the Answer_to_42.txt file"
-ELSE
-
-DO
-
-READ(44,55,IOSTAT = err) input
-55 FORMAT(A110)
-IF(err /= 0) THEN
-	EXIT
-END IF
-
-WRITE(*,*) input
-
-END DO
-CLOSE(44)
-END IF
+		CLOSE(44)
+	END IF
 END SUBROUTINE
 END MODULE
